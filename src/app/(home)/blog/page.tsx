42e76808c0cd47b37fbeafd112005b58c4605bcd
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-09-21 16:48:34 +0530

    simpler blog page

--- src/app/(home)/blog/page.tsx
+++ src/app/(home)/blog/page.tsx
@@ -8,31 +8,9 @@ export default function Page(): React.ReactElement {
       new Date(a.data.date ?? a.file.name).getTime(),
   );
 
-  const svg = `<svg viewBox='0 0 500 500' xmlns='http://www.w3.org/2000/svg'>
-  <filter id='noiseFilter'>
-    <feTurbulence 
-      type='fractalNoise' 
-      baseFrequency='0.65' 
-      numOctaves='3' 
-      stitchTiles='stitch'/>
-  </filter>
-  
-  <rect width='100%' height='100%' filter='url(#noiseFilter)'/>
-</svg>`;
-
   return (
     <main className="container max-sm:px-0 md:py-12">
-      <div
-        className="p-8"
-        style={{
-          backgroundImage: [
-            'radial-gradient(circle at 70% 10%, rgba(57, 1, 1, 0.5), transparent)',
-            'radial-gradient(circle at 0% 80%, rgba(124, 2, 2, 0.5), transparent)',
-            'radial-gradient(circle at 50% 50%, rgba(1, 1, 134, 0.3), transparent)',
-            `url("data:image/svg+xml,${encodeURIComponent(svg)}")`,
-          ].join(', '),
-        }}
-      >
+      <div className="p-8">
         <h1 className="border-fd-foreground mb-4 border-b-4 pb-2 text-4xl font-bold md:text-5xl">
           Frontend Hire Blog
         </h1>
